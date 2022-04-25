8c975ea5f3b99e768ae5e67b76228d4ee3fe36da
Aj - Thomas <thomascamlong@gmail.com>
2022-04-25 23:33:10 +0200

    Change page name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,14 +20,14 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>MyHomePage - Your new browser homepage!</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
         <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-          <NotificationsProvider>
+          <NotificationsProvider position='top-right'>
             <Layout>
               <Component {...pageProps} />
             </Layout>
