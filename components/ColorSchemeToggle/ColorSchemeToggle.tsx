a4fcea76d42afa6cadaacc113e8d21a1e6a89d26
Aj - Thomas <thomascamlong@gmail.com>
2022-04-25 23:33:19 +0200

    Edit ColorSchemeToggle

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { ActionIcon, Box, useMantineColorScheme } from '@mantine/core';
 import { Sun, MoonStars } from 'tabler-icons-react';
 import { motion } from 'framer-motion';
 
@@ -14,15 +14,15 @@ export function ColorSchemeToggle() {
         borderRadius: '100%',
       }}
     >
-      <ActionIcon
+      <Box
         onClick={() => toggleColorScheme()}
-        size="xl"
         sx={(theme) => ({
+          cursor: "pointer",
           color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
         })}
       >
         {colorScheme === 'dark' ? <Sun size={24} /> : <MoonStars size={24} />}
-      </ActionIcon>
+      </Box>
     </motion.div>
   );
 }
