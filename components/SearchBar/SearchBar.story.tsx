9690601efcbdc34d22a464f31768e1452be0d7ad
Aj - Thomas <thomascamlong@gmail.com>
2022-04-27 03:11:51 +0200

    Add stories

---
+++ components/SearchBar/SearchBar.story.tsx
@@ -0,0 +1,11 @@
+import SearchBar from "./SearchBar";
+
+
+export default {
+  title: 'Search bar',
+  config: {
+    searchBar: false,
+  },
+};
+
+export const Default = (args: any) => <SearchBar {...args} />;
---
+++ components/Settings/SettingsMenu.story.tsx
@@ -0,0 +1,10 @@
+import { SettingsMenuButton } from "./SettingsMenu";
+
+export default {
+  title: ' menu',
+  args: {
+    opened: false,
+  },
+};
+
+export const Default = (args: any) => <SettingsMenuButton {...args} />;
