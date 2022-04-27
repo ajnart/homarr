255142395b67a8fddd6da4c86ac73143041cbe8e
Aj - Thomas <thomascamlong@gmail.com>
2022-04-27 03:12:53 +0200

    Update index layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,16 @@ import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import AppShelf from '../components/AppShelf/AppShelf';
 import { Center } from '@mantine/core';
+import SaveConfigComponent from '../components/Config/SaveConfig';
+import LoadConfigComponent from '../components/Config/LoadConfig';
+import { useEffect, useState } from 'react';
+import { serviceItem } from '../components/AppShelf/AppShelf.d';
 
 export default function HomePage() {
   return (
     <>
-      <AppShelf/>
+      <AppShelf />
+      <LoadConfigComponent  />
     </>
   );
 }
