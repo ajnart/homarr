a72d9f0f557babd89c94bf5522d5462f250ac8c8
Aj - Thomas <thomascamlong@gmail.com>
2022-04-27 23:18:57 +0200

    Update SearchBar

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Input, TextInput, Text, ActionIcon, useMantineTheme } from '@mantine/core';
+import { Input, TextInput, Text, ActionIcon, useMantineTheme, Center } from '@mantine/core';
 import { useForm } from '@mantine/hooks';
 import { showNotification } from '@mantine/notifications';
 import { useState, useEffect } from 'react';
@@ -9,9 +9,11 @@ export default function SearchBar(props: any) {
   const theme = useMantineTheme();
   const [config, setConfig] = useState<Config>({
     searchBar: true,
-    searchUrl : 'https://www.google.com/search?q=',
+    searchUrl: 'https://www.google.com/search?q=',
   });
+
   const querryUrl = config.searchUrl || 'https://www.google.com/search?q=';
+
   const form = useForm({
     initialValues: {
       querry: '',
@@ -41,7 +43,6 @@ export default function SearchBar(props: any) {
         radius="xl"
         size="md"
         placeholder="Search the web"
-        rightSectionWidth={42}
         {...props}
         {...form.getInputProps('querry')}
       />
