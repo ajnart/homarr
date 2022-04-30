863262b1e565cc35989f1569694ad330d84894a2
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-04-30 21:35:14 +0200

    Added a simple API file in tools folder
    Will probably use NextJS api instead

---
+++ tools/api.ts
@@ -0,0 +1,9 @@
+import { serviceItem } from '../components/AppShelf/AppShelf.d';
+
+export function pingQbittorrent(service: serviceItem): any {
+  console.log("Getting service.cookie for service: ", service);
+  if (!service.cookie)
+    service.cookie = "Test";
+  else
+    console.log(service.cookie);
+}
