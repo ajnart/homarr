17ebef6c589a7a87ce9a6375ee059aa41c908836
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-04-30 21:37:36 +0200

    Edit AppShelfMenu
    Preparing to add an Edit button

--- components/AppShelf/AppShelfMenu.tsx
+++ components/AppShelf/AppShelfMenu.tsx
@@ -1,13 +1,25 @@
 import { Menu, Text } from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
-import { Check, Trash } from 'tabler-icons-react';
+import { Check, Edit, Trash } from 'tabler-icons-react';
 
 export default function AppShelfMenu(props: any) {
   const { name, removeitem: removeItem } = props;
   return (
     <Menu sx={{ position: 'absolute', top: 3, right: 3 }}>
       <Menu.Label>Settings</Menu.Label>
-
+      <Menu.Item
+        color="primary"
+        icon={<Edit size={14} />}
+        onClick={() => {
+          showNotification({
+            color: 'red',
+            title: <Text>Feature not yet implemented</Text>,
+            message: `${name} could not be edited`,
+          });
+        }}
+      >
+        Rename
+      </Menu.Item>
       <Menu.Label>Danger zone</Menu.Label>
       <Menu.Item
         color="red"
