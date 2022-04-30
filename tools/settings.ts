61df004ace77a19734de84f69545b2dbafb92553
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-04-30 21:34:28 +0200

    Rename config to settings

--- tools/settings.ts
+++ tools/settings.ts
@@ -1,17 +1,17 @@
-export function loadConfig(path: string): Config | null {
+export function loadSettings(path: string): Settings | null {
   const item = localStorage.getItem(path);
   if (!item) {
     return null;
   }
 
   try {
-    return JSON.parse(item) as Config;
+    return JSON.parse(item) as Settings;
   } catch (e) {
     return null;
   }
 }
 
-export interface Config {
+export interface Settings {
   searchUrl: string;
 	searchBar: boolean,
 }
