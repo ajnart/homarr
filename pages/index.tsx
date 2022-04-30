a7a72cf9fee174b69608ed7d3ab4ffa6ad18410d
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-04-30 21:35:23 +0200

    Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
+import { Notification } from '@mantine/core';
 import AppShelf from '../components/AppShelf/AppShelf';
-import { Center, Group } from '@mantine/core';
 import LoadConfigComponent from '../components/Config/LoadConfig';
 import SearchBar from '../components/SearchBar/SearchBar';
 
