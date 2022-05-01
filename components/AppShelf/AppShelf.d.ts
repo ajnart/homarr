07449b92728126ec0b5fd782d81368ad304627dd
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-05-01 14:46:15 +0200

    Delete old types

--- components/AppShelf/AppShelf.d.ts
+++
@@ -1,9 +0,0 @@
-export const ServiceTypes = ['Other', 'Sonarr', 'Radarr', 'Lidarr', 'qBittorrent', 'Plex', 'Emby'];
-
-export interface serviceItem {
-  [x: string]: any;
-  name: string;
-  type: ServiceTypes;
-  url: string;
-  icon: string;
-}
