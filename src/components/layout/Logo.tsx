59a1379e4982601cb085ad74dc4e30218dbad2e4
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-05-15 12:42:53 +0200

    :bento: Update logo and favicon

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,10 +1,18 @@
-import { Group, Text } from '@mantine/core';
+import { Group, Image, Text } from '@mantine/core';
 import * as React from 'react';
 import { CURRENT_VERSION } from '../../../data/constants';
 
 export function Logo({ style }: any) {
   return (
     <Group>
+      <Image
+        width={50}
+        src="/imgs/logo.png"
+        style={{
+          position: 'relative',
+          left: 15,
+        }}
+      />
       <Text
         sx={style}
         weight="bold"
@@ -15,11 +23,14 @@ export function Logo({ style }: any) {
       </Text>
       <Text
         style={{
+          position: 'relative',
+          left: -14,
+          bottom: -2,
           color: 'gray',
           fontStyle: 'inherit',
           fontSize: 'inherit',
-          alignSelf: 'end',
-          alignContent: 'start',
+          alignSelf: 'center',
+          alignContent: 'center',
         }}
       >
         {CURRENT_VERSION}
