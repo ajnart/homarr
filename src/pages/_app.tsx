027ac94e8046e51f7932da25190641b04d3c28f7
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-05-15 12:42:53 +0200

    :bento: Update logo and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { getCookie, setCookies } from 'cookies-next';
 import Head from 'next/head';
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import { useHotkeys } from '@mantine/hooks';
 import Layout from '../components/layout/Layout';
 import { ConfigProvider } from '../tools/state';
 import { theme } from '../tools/theme';
@@ -18,13 +19,14 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
     setColorScheme(nextColorScheme);
     setCookies('color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
   };
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
 
   return (
     <>
       <Head>
         <title>Homarr - A homepage for your server!</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
