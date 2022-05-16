11b44e5d542f1e142fb5d1dde54a083537d34c98
Aj - Thomas <thomascamlong@gmail.com>
2022-05-16 13:54:29 +0200

    🔥 Remove Navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,31 +1,28 @@
-import { AppShell, Center, createStyles } from '@mantine/core';
+import { AppShell, createStyles } from '@mantine/core';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import Aside from './Aside';
-import Navbar from './Navbar';
 
 const useStyles = createStyles((theme) => ({
-  main: {
-  },
+  main: {},
 }));
 
 export default function Layout({ children, style }: any) {
   const { classes, cx } = useStyles();
   return (
     <AppShell
-      navbar={<Navbar />}
       aside={<Aside />}
-      header={<Header links={[]} />}
+      header={<Header />}
       footer={<Footer links={[]} />}
     >
-        <main
-          className={cx(classes.main)}
-          style={{
-            ...style,
-          }}
-        >
-          {children}
-        </main>
+      <main
+        className={cx(classes.main)}
+        style={{
+          ...style,
+        }}
+      >
+        {children}
+      </main>
     </AppShell>
   );
 }
