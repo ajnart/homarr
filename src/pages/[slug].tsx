ace3e6da3cbcbdd7a1da7eac0089c261248eaf2b
ajnart <thomascamlong@gmail.com>
2022-05-21 00:55:11 +0200

    :sparkles: Match config with URL typed
    Homarr will now match a config with the URL used or return a 404 if not found

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,8 +1,54 @@
-import { Title } from '@mantine/core';
-import { useRouter } from 'next/router';
+import { GetServerSidePropsContext } from 'next';
+import path from 'path';
+import fs from 'fs';
+import { useEffect } from 'react';
+import AppShelf from '../components/AppShelf/AppShelf';
+import LoadConfigComponent from '../components/Config/LoadConfig';
+import { Config } from '../tools/types';
+import { useConfig } from '../tools/state';
 
-export default function SlugPage(props: any) {
-  const router = useRouter();
-  const { slug } = router.query;
-  return <Title>ok</Title>;
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<{ props: { config: Config } }> {
+  const configByUrl = context.query.slug;
+  const configPath = path.join(process.cwd(), 'data/configs', `${configByUrl}.json`);
+  const configExists = fs.existsSync(configPath);
+  if (!configExists) {
+    // Redirect to 404
+    context.res.writeHead(301, { Location: '/404' });
+    context.res.end();
+    return {
+      props: {
+        config: {
+          name: '',
+          services: [],
+          settings: {
+            enabledModules: [],
+            searchUrl: 'https://www.google.com/search?q=',
+          },
+        },
+      },
+    };
+  }
+  const config = fs.readFileSync(configPath, 'utf8');
+  // Print loaded config
+  return {
+    props: {
+      config: JSON.parse(config),
+    },
+  };
+}
+
+export default function HomePage(props: any) {
+  const { config: initialConfig }: { config: Config } = props;
+  const { setConfig } = useConfig();
+  useEffect(() => {
+    setConfig(initialConfig);
+  }, [initialConfig]);
+  return (
+    <>
+      <AppShelf />
+      <LoadConfigComponent />
+    </>
+  );
 }
