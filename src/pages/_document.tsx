861bf62b1ebb4303621792c2c7ccd4580b2cd943
ajnart <thomascamlong@gmail.com>
2022-05-21 10:28:19 +0200

    :sparkles: Basic drag and drop

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,14 @@
 import Document, { DocumentContext } from 'next/document';
 import { ServerStyles, createStylesServer } from '@mantine/next';
+import { resetServerContext } from 'react-beautiful-dnd';
 
 const stylesServer = createStylesServer();
 
 export default class _Document extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
+    resetServerContext();
 
+    const initialProps = await Document.getInitialProps(ctx);
     // Add your app specific logic here
 
     return {
