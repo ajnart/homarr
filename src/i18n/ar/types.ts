18ccad7ca43d6e97b54ef5e86a07f28df0a3226f
Thomas Camlong <49837342+ajnart@users.noreply.github.com>
2022-05-25 18:21:08 +0200

    New translations types.ts (Arabic)

---
+++ src/i18n/ar/types.ts
@@ -0,0 +1,29 @@
+import { OptionValues } from '../components/modules/modules';
+export interface Settings {
+  searchUrl: string;
+}
+export interface Config {
+  name: string;
+  services: serviceItem[];
+  settings: Settings;
+  modules: {
+    [key: string]: ConfigModule;
+  };
+}
+interface ConfigModule {
+  title: string;
+  enabled: boolean;
+  options: {
+    [key: string]: OptionValues;
+  };
+}
+export const ServiceTypeList = ['Other', 'Emby', 'Lidarr', 'Plex', 'Radarr', 'Readarr', 'Sonarr', 'qBittorrent'];
+export type ServiceType = 'Other' | 'Emby' | 'Lidarr' | 'Plex' | 'Radarr' | 'Readarr' | 'Sonarr' | 'qBittorrent';
+export interface serviceItem {
+  id: string;
+  name: string;
+  type: string;
+  url: string;
+  icon: string;
+  apiKey?: string;
+}
