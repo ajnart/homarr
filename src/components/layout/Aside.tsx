ad1af0e07dc48c37690df223133c65303b727b3d
ajnart <ajnart@pm.me>
2022-05-29 15:30:23 +0200

    :bricks: Move components in infrastructure

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -1,5 +1,5 @@
 import { Aside as MantineAside, Group } from '@mantine/core';
-import { WeatherModule, DateModule, CalendarModule } from '../modules';
+import { WeatherModule, DateModule, CalendarModule, TotalDownloadsModule } from '../modules';
 import { ModuleWrapper } from '../modules/moduleWrapper';
 
 export default function Aside(props: any) {
@@ -15,10 +15,11 @@ export default function Aside(props: any) {
         base: 'auto',
       }}
     >
-      <Group mt="sm" grow direction="column">
+      <Group mt="sm" grow direction="column" style={{ width: 300 }}>
         <ModuleWrapper module={CalendarModule} />
-        <ModuleWrapper module={DateModule} />
+        <ModuleWrapper module={TotalDownloadsModule} />
         <ModuleWrapper module={WeatherModule} />
+        <ModuleWrapper module={DateModule} />
       </Group>
     </MantineAside>
   );
