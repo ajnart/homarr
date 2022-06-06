c2fd14d04ab24bac09cd98fec3feb379bc35115b
ajnart <ajnart@pm.me>
2022-06-06 15:02:41 +0200

    🪝 AdduseSetSafeInterval hook

---
+++ src/tools/hooks/useSetSafeInterval.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+export function useSetSafeInterval() {
+  const timers = useRef<NodeJS.Timer[]>([]);
+
+  function setSafeInterval(callback: () => void, delay: number) {
+    const newInterval = setInterval(callback, delay);
+    timers.current.push(newInterval);
+    return newInterval;
+  }
+
+  useEffect(
+    () => () => {
+      timers.current.forEach((t) => {
+        clearInterval(t);
+      });
+    },
+    []
+  );
+
+  return setSafeInterval;
+}
