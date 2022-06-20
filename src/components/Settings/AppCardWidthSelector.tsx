14a40d9f6654e267349cb5d98fb7384de56fed79
ajnart <ajnart@pm.me>
2022-06-20 10:24:22 +0200

    🔧 Tweak values and UI changes

--- src/components/Settings/AppCardWidthSelector.tsx
+++ src/components/Settings/AppCardWidthSelector.tsx
@@ -5,15 +5,6 @@ import { useConfig } from '../../tools/state';
 export function AppCardWidthSelector() {
   const { config, setConfig } = useConfig();
 
-  const MARKS = [
-    { value: 0.8 },
-    { value: 1 },
-    { value: 1.2 },
-    { value: 1.4 },
-    { value: 1.6 },
-    { value: 2 },
-  ];
-
   const setappCardWidth = (appCardWidth: number) => {
     setConfig({
       ...config,
@@ -33,7 +24,6 @@ export function AppCardWidthSelector() {
         step={0.2}
         min={0.8}
         max={2}
-        marks={MARKS}
         styles={{ markLabel: { fontSize: 'xx-small' } }}
         onChange={(value) => setappCardWidth(value)}
       />
