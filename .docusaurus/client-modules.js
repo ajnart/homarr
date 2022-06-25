1ae074db8f1adc4fae30cd73636c76daf9a5b094
Thomas Camlong <ajnart@pm.me>
2022-06-25 17:51:44 +0200

    :fire: Remove .docusaurus/

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/ajna/Documents/GitHub/homarr/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/ajna/Documents/GitHub/homarr/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/ajna/Documents/GitHub/homarr/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/ajna/Documents/GitHub/homarr/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/ajna/Documents/GitHub/homarr/src/css/custom.css'),
-];
