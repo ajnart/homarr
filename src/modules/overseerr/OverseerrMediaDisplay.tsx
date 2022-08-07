70814d0bc699810f9e1eb3bb07eb9b5dd42c9a2f
ajnart <ajnart@pm.me>
2022-08-07 12:16:29 +0200

    :sparkles: Add Overseerr integration

--- src/modules/overseerr/OverseerrMediaDisplay.tsx
+++ src/modules/overseerr/OverseerrMediaDisplay.tsx
@@ -1,18 +1,18 @@
 import { MediaDisplay } from '../common';
+import { Result } from './SearchResult';
 
 export default function OverseerrMediaDisplay(props: any) {
-  const { media }: { media: any } = props;
+  const { media }: { media: Result } = props;
   return (
     <MediaDisplay
       media={{
-        title: media.name ?? media.originalTitle,
-        overview: media.overview,
+        title: media.name ?? media.originalTitle ?? media.title ?? '',
         poster: `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${media.posterPath}`,
         genres: [`score: ${media.voteAverage}/10`],
         seasonNumber: media.mediaInfo?.seasons.length,
         plexUrl: media.mediaInfo?.plexUrl,
         imdbId: media.mediaInfo?.imdbId,
-        ...media,
+        overview: media.overview,
       }}
     />
   );
