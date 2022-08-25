784c745e9461001ec31d04003aebd40783779c66
ajnart <ajnart@pm.me>
2022-08-25 12:59:57 +0200

    :globe_with_meridians: Add languages to i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   debug: process.env.NODE_ENV === 'development',
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de'],
+    locales: ['en', 'de', 'es', 'fr', 'it', 'ja', 'nl', 'ru', 'sv', 'zh'],
     localeDetection: true,
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
