b9ff12ff0d5949443b4a7c6c180f818737d69fa4
Jannes Vandepitte <jannes.vandepitte@gmail.com>
2022-08-26 10:46:34 +0200

    History done

--- src/pages/api/modules/usenet/index.ts
+++ src/pages/api/modules/usenet/index.ts
@@ -5,45 +5,63 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Client } from 'sabnzbd-api';
 import { UsenetQueueItem } from '../../../../modules';
 import { getConfig } from '../../../../tools/getConfig';
+import { getServiceById } from '../../../../tools/hooks/useGetServiceByType';
 import { Config } from '../../../../tools/types';
 
 dayjs.extend(duration);
+
+export interface UsenetQueueRequestParams {
+  serviceId: string;
+  offset: number;
+  limit: number;
+}
+
+export interface UsenetQueueResponse {
+  items: UsenetQueueItem[];
+  total: number;
+}
 
 async function Get(req: NextApiRequest, res: NextApiResponse) {
   try {
     const configName = getCookie('config-name', { req });
     const { config }: { config: Config } = getConfig(configName?.toString() ?? 'default').props;
-    const nzbServices = config.services.filter((service) => service.type === 'Sabnzbd');
+    const { limit, offset, serviceId } = req.query as any as UsenetQueueRequestParams;
 
-    const downloads: UsenetQueueItem[] = [];
+    const service = getServiceById(config, serviceId);
 
-    await Promise.all(
-      nzbServices.map(async (service) => {
-        if (!service.apiKey) {
-          throw new Error(`API Key for service "${service.name}" is missing`);
-        }
-        const queue = await new Client(service.url, service.apiKey).queue();
+    if (!service) {
+      throw new Error(`Service with ID "${req.query.serviceId}" could not be found.`);
+    }
 
-        queue.slots.forEach((slot) => {
-          const [hours, minutes, seconds] = slot.timeleft.split(':');
-          const eta = dayjs.duration({
-            hour: parseInt(hours, 10),
-            minutes: parseInt(minutes, 10),
-            seconds: parseInt(seconds, 10),
-          } as any);
-          downloads.push({
-            id: slot.nzo_id,
-            eta: eta.asSeconds(),
-            name: slot.filename,
-            progress: parseFloat(slot.percentage),
-            size: parseFloat(slot.mb) * 1000 * 1000,
-            state: slot.status.toLowerCase() as any,
-          });
-        });
-      })
-    );
+    if (!service.apiKey) {
+      throw new Error(`API Key for service "${service.name}" is missing`);
+    }
+    const queue = await new Client(service.url, service.apiKey).queue(offset, limit);
 
-    return res.status(200).json(downloads);
+    const items: UsenetQueueItem[] = queue.slots.map((slot) => {
+      const [hours, minutes, seconds] = slot.timeleft.split(':');
+      const eta = dayjs.duration({
+        hour: parseInt(hours, 10),
+        minutes: parseInt(minutes, 10),
+        seconds: parseInt(seconds, 10),
+      } as any);
+
+      return {
+        id: slot.nzo_id,
+        eta: eta.asSeconds(),
+        name: slot.filename,
+        progress: parseFloat(slot.percentage),
+        size: parseFloat(slot.mb) * 1000 * 1000,
+        state: slot.status.toLowerCase() as any,
+      };
+    });
+
+    const response: UsenetQueueResponse = {
+      items,
+      total: queue.noofslots_total,
+    };
+
+    return res.status(200).json(response);
   } catch (err) {
     return res.status(401).json(err);
   }
