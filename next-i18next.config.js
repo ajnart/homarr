cbba694a3c4b88566322491953c8eedc1eb1d94b
Thomas Camlong <49837342+ajnart@users.noreply.github.com>
2022-08-27 19:26:48 +0200

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de', 'es', 'fr', 'it', 'ja', 'nl', 'ru', 'sv', 'zh'],
+    locales: ['en', 'de', 'en', 'es', 'fr', 'it', 'ja', 'nl', 'pl', 'ru', 'sl', 'sv', 'zh'],
     localeDetection: true,
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
