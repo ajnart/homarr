7fc42a7579179b4c2d50a6789525f356b1200878
Manuel <manuel.ruwe@bluewin.ch>
2022-09-12 21:42:06 +0200

    🌐 fix slug and login page translations

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,23 +1,30 @@
-import { GetServerSidePropsContext } from 'next';
-import path from 'path';
+import { getCookie } from 'cookies-next';
 import fs from 'fs';
+import { GetServerSidePropsContext } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import path from 'path';
 import { useEffect } from 'react';
 import AppShelf from '../components/AppShelf/AppShelf';
 import LoadConfigComponent from '../components/Config/LoadConfig';
-import { Config } from '../tools/types';
-import { useConfig } from '../tools/state';
 import Layout from '../components/layout/Layout';
+import { getConfig } from '../tools/getConfig';
+import { useConfig } from '../tools/state';
+import { dashboardNamespaces } from '../tools/translation-namespaces';
+import { Config } from '../tools/types';
 
-export async function getServerSideProps(
-  context: GetServerSidePropsContext
-): Promise<{ props: { config: Config } }> {
-  const configByUrl = context.query.slug;
+export async function getServerSideProps({
+  req,
+  res,
+  locale,
+  query,
+}: GetServerSidePropsContext): Promise<{ props: { config: Config } }> {
+  const configByUrl = query.slug;
   const configPath = path.join(process.cwd(), 'data/configs', `${configByUrl}.json`);
   const configExists = fs.existsSync(configPath);
   if (!configExists) {
     // Redirect to 404
-    context.res.writeHead(301, { Location: '/404' });
-    context.res.end();
+    res.writeHead(301, { Location: '/404' });
+    res.end();
     return {
       props: {
         config: {
@@ -31,13 +38,11 @@ export async function getServerSideProps(
       },
     };
   }
-  const config = fs.readFileSync(configPath, 'utf8');
-  // Print loaded config
-  return {
-    props: {
-      config: JSON.parse(config),
-    },
-  };
+
+  const configLocale = getCookie('config-locale', { req, res });
+  const targetLanguage = (configLocale ?? locale) as string;
+  const translations = await serverSideTranslations(targetLanguage, dashboardNamespaces);
+  return getConfig(configByUrl as string, translations);
 }
 
 export default function HomePage(props: any) {
