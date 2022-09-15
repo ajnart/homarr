4ed7011a34752d7d8a3db58f55d4bfa83e28689e
Jannes Vandepitte <jannes.vandepitte@gmail.com>
2022-09-15 13:04:47 +0200

    Remove controllers

--- src/server/calendar/calendar.module.ts
+++ src/server/calendar/calendar.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RadarrModule } from '../radarr/radarr.module';
 import { SonarrModule } from '../sonarr/sonarr.module';
-import { CalendarController } from './calendar.controller';
 import { CalendarResolver } from './calendar.resolver';
 
 @Module({
   providers: [CalendarResolver],
-  controllers: [CalendarController],
   imports: [RadarrModule, SonarrModule],
 })
 export class CalendarModule {}
--- src/server/configs/config.module.ts
+++ src/server/configs/config.module.ts
@@ -1,11 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigService } from './config.service';
-import { ConfigController } from './config.controller';
 import { ConfigResolver } from './config.resolver';
 
 @Global()
 @Module({
-  controllers: [ConfigController],
   providers: [ConfigService, ConfigResolver],
   exports: [ConfigService],
 })
