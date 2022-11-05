2ccc73da1eab45d084338fb41d3754bde1e28a78
Jon Terry <jonathan.terry@rsa.com>
2022-11-05 14:08:47 -0500

    ✨ Add NZBGet download client

---
+++ src/pages/api/modules/usenet/nzbget/nzbget-client.ts
@@ -0,0 +1,25 @@
+import { NzbgetClientOptions } from "./types";
+import NZBGet from 'nzbget-api';
+
+export function NzbgetClient(options: NzbgetClientOptions) {
+    if (!options?.host) {
+        throw new Error('Cannot connect to NZBGet. Missing host in service config.')
+    }
+
+    if (!options?.port) {
+        throw new Error('Cannot connect to NZBGet. Missing port in service config.')
+    }
+
+    if (!options?.login) {
+        throw new Error('Cannot connect to NZBGet. Missing username in service config.')
+    }
+
+    if (!options?.hash) {
+        throw new Error('Cannot connect to NZBGet. Missing password in service config.')
+    }
+    
+    return new NZBGet(options);
+};
+
+
+  
