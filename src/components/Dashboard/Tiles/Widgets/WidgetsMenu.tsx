a8dea274f37418d28b66db120d08d8ad1efbdc8e
Manuel <30572287+manuel-rw@users.noreply.github.com>
2023-02-05 21:41:47 +0100

    ♻️ Display of available properties in menu

--- src/components/Dashboard/Tiles/Widgets/WidgetsMenu.tsx
+++ src/components/Dashboard/Tiles/Widgets/WidgetsMenu.tsx
@@ -1,12 +1,12 @@
 import { Title } from '@mantine/core';
 import { useTranslation } from 'next-i18next';
 import { openContextModalGeneric } from '../../../../tools/mantineModalManagerExtensions';
+import WidgetsDefinitions from '../../../../widgets';
 import { IWidget } from '../../../../widgets/widgets';
 import { useWrapperColumnCount } from '../../Wrappers/gridstack/store';
 import { GenericTileMenu } from '../GenericTileMenu';
 import { WidgetEditModalInnerProps } from './WidgetsEditModal';
 import { WidgetsRemoveModalInnerProps } from './WidgetsRemoveModal';
-import WidgetsDefinitions from '../../../../widgets';
 
 export type WidgetChangePositionModalInnerProps = {
   widgetId: string;
@@ -76,7 +76,8 @@ export const WidgetsMenu = ({ integration, widget }: WidgetsMenuProps) => {
       handleClickChangePosition={handleChangeSizeClick}
       handleClickDelete={handleDeleteClick}
       displayEdit={
-        typeof widget.properties !== 'undefined' && Object.keys(widget.properties).length !== 0
+        typeof widget.properties !== 'undefined' &&
+        Object.keys(widgetDefinitionObject?.options ?? {}).length !== 0
       }
     />
   );
