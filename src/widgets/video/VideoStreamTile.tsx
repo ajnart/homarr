76ba0cedc693f50d12ade0842bb58ecd8ab71399
ajnart <ajnart@pm.me>
2023-02-10 12:44:31 +0900

    ⚡️ Change code editor

--- src/widgets/video/VideoStreamTile.tsx
+++ src/widgets/video/VideoStreamTile.tsx
@@ -1,6 +1,6 @@
 import { Center, Group, Stack, Title } from '@mantine/core';
 import { IconDeviceCctv, IconHeartBroken } from '@tabler/icons';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'next-i18next';
 import { defineWidget } from '../helper';
 import { IWidget } from '../widgets';
 import VideoFeed from './VideoFeed';
