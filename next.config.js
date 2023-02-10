748143056fa87e90abf60b149d969c4aedfdeddf
ajnart <ajnart@pm.me>
2023-02-10 12:44:48 +0900

    ➕ Remove unsed dependencies and add new css editor

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 const { i18n } = require('./next-i18next.config');
 
-const removeImports = require('next-remove-imports')();
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer(
-  removeImports({
-    experimental: { esmExternals: true },
-    images: {
-      domains: ['cdn.jsdelivr.net'],
-    },
-    reactStrictMode: true,
-    output: 'standalone',
-    i18n,
-  })
-);
+module.exports = withBundleAnalyzer({
+  images: {
+    domains: ['cdn.jsdelivr.net'],
+  },
+  reactStrictMode: true,
+  output: 'standalone',
+  i18n,
+});
