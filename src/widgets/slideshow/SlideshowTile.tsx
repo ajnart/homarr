4f996b95317c7d7b99db39160eb0dc1048e9f1b9
hillaliy <hillaliy@me.com>
2023-02-19 07:55:39 +0200

    SildeshowWidget

--- src/widgets/slideshow/SlideshowTile.tsx
+++ src/widgets/slideshow/SlideshowTile.tsx
@@ -2,7 +2,7 @@
 import { Center, Stack, Title, createStyles, Image } from '@mantine/core';
 import { IconPhoto, IconPhotoX } from '@tabler/icons';
 import { useTranslation } from 'next-i18next';
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import { Carousel } from '@mantine/carousel';
 import Autoplay from 'embla-carousel-autoplay';
 import { defineWidget } from '../helper';
@@ -12,7 +12,7 @@ const definition = defineWidget({
   id: 'slideshow',
   icon: IconPhoto,
   options: {
-    feedUrl: {
+    imagesPath: {
       type: 'text',
       defaultValue: '',
     },
@@ -23,7 +23,7 @@ const definition = defineWidget({
   },
   gridstack: {
     minWidth: 3,
-    minHeight: 2,
+    minHeight: 3,
     maxWidth: 12,
     maxHeight: 12,
   },
@@ -34,6 +34,55 @@ export type SlideshowWidget = IWidget<(typeof definition)['id'], typeof definiti
 
 interface SlideshowWidgetProps {
   widget: SlideshowWidget;
+}
+
+function SlideshowWidget({ widget }: SlideshowWidgetProps) {
+  const { t } = useTranslation('modules/slideshow');
+  const { classes } = useStyles();
+  const autoplay = useRef(Autoplay({ delay: widget.properties.duration }));
+  const cache: any = {};
+
+  if (!widget.properties.imagesPath) {
+    return (
+      <Center h="100%">
+        <Stack align="center">
+          <IconPhotoX />
+          <Title order={4}>{t('errors.invalidUrl')}</Title>
+        </Stack>
+      </Center>
+    );
+  }
+
+  function importAll(r: any) {
+    // eslint-disable-next-line no-return-assign
+    r.keys().forEach((key: any) => (cache[key] = r(key)));
+  }
+
+  importAll(require.context('./media', false, /\.(png|jpe?g|svg)$/));
+  const images = Object.entries(cache).map((module: any) => module[1].default);
+
+  const slides = images.map((image) => (
+    <Carousel.Slide>
+      <Image src={image} />
+    </Carousel.Slide>
+  ));
+
+  return (
+    <div style={{ padding: 40, maxWidth: 500 }}>
+      <Carousel
+        mx="auto"
+        loop
+        controlSize={25}
+        height={400}
+        plugins={[autoplay.current]}
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+        classNames={classes}
+      >
+        {slides}
+      </Carousel>
+    </div>
+  );
 }
 
 const useStyles = createStyles((_theme, _params, getRef) => ({
@@ -52,41 +101,4 @@ const useStyles = createStyles((_theme, _params, getRef) => ({
   },
 }));
 
-function SlideshowWidget({ widget }: SlideshowWidgetProps) {
-  const { t } = useTranslation('modules/slideshow');
-  const { classes } = useStyles();
-  const autoplay = useRef(Autoplay({ delay: 5000 }));
-  const images: any[] = [widget.properties.feedUrl];
-  const slides = images.map((url) => (
-    <Carousel.Slide key={url}>
-      <Image src={url} />
-    </Carousel.Slide>
-  ));
-
-  if (!widget.properties.feedUrl) {
-    return (
-      <Center h="100%">
-        <Stack align="center">
-          <IconPhotoX />
-          <Title order={4}>{t('errors.invalidUrl')}</Title>
-        </Stack>
-      </Center>
-    );
-  }
-  return (
-    <Carousel
-      sx={{ maxWidth: '100%' }}
-      mx="auto"
-      loop
-      controlSize={25}
-      height="450px"
-      plugins={[autoplay.current]}
-      onMouseEnter={autoplay.current.stop}
-      onMouseLeave={autoplay.current.reset}
-      classNames={classes}
-    >
-      {slides}
-    </Carousel>
-  );
-}
 export default definition;
