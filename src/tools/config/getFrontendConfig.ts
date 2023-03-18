2063adc6c0d9b3951bef6b0aa4b34348a742e1c6
ajnart <ajnart@pm.me>
2023-03-18 18:29:22 +0800

    ✨ Add console warn if apps are missing properties

--- src/tools/config/getFrontendConfig.ts
+++ src/tools/config/getFrontendConfig.ts
@@ -7,6 +7,22 @@ export const getFrontendConfig = (name: string): ConfigType => {
   const config = getConfig(name);
 
   Consola.info(`Requested frontend content of configuration '${name}'`);
+  // Find out if config as apps with integrations that have a property with no value or undefined
+  // If so, remove print an error using consola
+  // If not, return the config
+  const appsWithIntegrationsWithUndefinedProperties = config.apps.filter(
+    (app) =>
+      app.integration?.properties.some(
+        (property) => property.value === null || property.value === undefined
+      ) ?? false
+  );
+  if (appsWithIntegrationsWithUndefinedProperties.length > 0) {
+    Consola.warn(
+      `The following apps have integrations with errored properties: [${appsWithIntegrationsWithUndefinedProperties
+        .map((app) => app.name)
+        .join(', ')}] please input the correct secrets once again for the concerned app(s), save them, exit edit mode and reload the page.`
+    );
+  }
 
   return {
     ...config,
