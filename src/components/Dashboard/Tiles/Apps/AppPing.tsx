57e8588c0d639db088ee4ace4cc490f2a049ba44
Manuel <30572287+manuel-rw@users.noreply.github.com>
2023-04-16 20:47:10 +0000

    fix typo

--- src/components/Dashboard/Tiles/Apps/AppPing.tsx
+++ src/components/Dashboard/Tiles/Apps/AppPing.tsx
@@ -19,7 +19,7 @@ export const AppPing = ({ app }: AppPingProps) => {
     queryKey: ['ping', { id: app.id, name: app.name }],
     queryFn: async () => {
       const response = await fetch(`/api/modules/ping?url=${encodeURI(app.url)}`);
-      const isOk = app.network.okStatus !== undefined ? app.network.okStatus.includes(response.statu) : app.network.statusCodes.includes(response.status.toString());
+      const isOk = app.network.okStatus !== undefined ? app.network.okStatus.includes(response.status) : app.network.statusCodes.includes(response.status.toString());
       return {
         status: response.status,
         state: isOk ? 'online' : 'down',
@@ -40,7 +40,7 @@ export const AppPing = ({ app }: AppPingProps) => {
       }}
       transition={{ repeat: Infinity, duration: 2.5, ease: 'easeInOut' }}
     >
-      <Tooltip
+      <TooltipTooltip
         withinPortal
         radius="lg"
         label={
