413f4e6a7f54a2210566f26e110861a6302d66f2
Manuel <30572287+manuel-rw@users.noreply.github.com>
2023-04-17 18:11:01 +0200

    Update migrateConfig.ts

--- src/tools/config/migrateConfig.ts
+++ src/tools/config/migrateConfig.ts
@@ -160,7 +160,7 @@ const migrateService = (oldService: serviceItem, areaType: AreaType): ConfigAppT
   network: {
     enabledStatusChecker: oldService.ping ?? true,
     statusCodes: oldService.status ?? ['200'],
-    okStatus: oldService.status.map((x) => Number(x)) ?? [200],
+    okStatus: oldService.status?.map((x) => Number(x)) ?? [200],
   },
   appearance: {
     iconUrl: migrateIcon(oldService.icon),
