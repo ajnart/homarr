3bce40c15e81241cb2eb644502fd423c7c5c5ba6
Manuel <30572287+manuel-rw@users.noreply.github.com>
2023-04-19 11:23:32 +0200

    Update AppPing.tsx

--- src/components/Dashboard/Tiles/Apps/AppPing.tsx
+++ src/components/Dashboard/Tiles/Apps/AppPing.tsx
@@ -1,4 +1,5 @@
 import { Indicator, Tooltip } from '@mantine/core';
+import Consola from 'consola';
 import { useQuery } from '@tanstack/react-query';
 import { motion } from 'framer-motion';
 import { useTranslation } from 'next-i18next';
@@ -19,9 +20,7 @@ export const AppPing = ({ app }: AppPingProps) => {
     queryKey: ['ping', { id: app.id, name: app.name }],
     queryFn: async () => {
       const response = await fetch(`/api/modules/ping?url=${encodeURI(app.url)}`);
-      const isOk = app.network.okStatus === undefined || app.network.statusCodes.length >= 1 ?
-app.network.statusCodes.includes(response.status.toString()) :
-app.network.okStatus.includes(response.status);
+      const isOk = getIsOk(app, response.status);
       return {
         status: response.status,
         state: isOk ? 'online' : 'down',
@@ -62,3 +61,12 @@ app.network.okStatus.includes(response.status);
     </motion.div>
   );
 };
+
+const getIsOk = (app: AppType, status: number) => {
+if (app.network.okStatus === undefined || app.network.statusCodes.length >= 1) {
+Consola.log('Using new status codes');
+return app.network.statusCodes.includes(status.toString());
+}
+Consola.warn('Using deprecated okStatus');
+return app.network.okStatus.includes(status);
+}
