7d04db539844405824f1a51684d5cf2b707d37e2
Meier Lukas <meierschlumpf@gmail.com>
2023-04-29 14:33:22 +0200

    ➕ Add prisma

---
+++ src/server/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
