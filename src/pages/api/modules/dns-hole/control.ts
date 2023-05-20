fb52c4b003e4012529d4c97a33c16e43ecdb8d7f
Manuel <30572287+manuel-rw@users.noreply.github.com>
2023-05-20 14:42:15 +0200

    ✨ Add ad guard home (#937)
    * ✨ Add add guard home
    
    * ✨ Add request for blocked domains and fix request for blocked queries
    
    * ♻️ PR feedback
    
    * ✅ Fix tests

--- src/pages/api/modules/dns-hole/control.ts
+++ src/pages/api/modules/dns-hole/control.ts
@@ -5,6 +5,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getConfig } from '../../../../tools/config/getConfig';
 import { findAppProperty } from '../../../../tools/client/app-properties';
 import { PiHoleClient } from '../../../../tools/server/sdk/pihole/piHole';
+import { ConfigAppType } from '../../../../types/app';
+import { AdGuard } from '../../../../tools/server/sdk/adGuard/adGuard';
 
 const getQuerySchema = z.object({
   status: z.enum(['enabled', 'disabled']),
@@ -21,27 +23,48 @@ export const Post = async (request: NextApiRequest, response: NextApiResponse) =
     return;
   }
 
-  const applicableApps = config.apps.filter((x) => x.integration?.type === 'pihole');
+  const applicableApps = config.apps.filter(
+    (x) => x.integration?.type && ['pihole', 'adGuardHome'].includes(x.integration?.type)
+  );
 
   for (let i = 0; i < applicableApps.length; i += 1) {
     const app = applicableApps[i];
 
-    const pihole = new PiHoleClient(
-      app.url,
-      findAppProperty(app, 'password')
-    );
-
-    switch (parseResult.data.status) {
-      case 'enabled':
-        await pihole.enable();
-        break;
-      case 'disabled':
-        await pihole.disable();
-        break;
+    if (app.integration?.type === 'pihole') {
+      await processPiHole(app, parseResult.data.status === 'disabled');
+      return;
     }
+
+    await processAdGuard(app, parseResult.data.status === 'disabled');
   }
 
   response.status(200).json({});
+};
+
+const processAdGuard = async (app: ConfigAppType, enable: boolean) => {
+  const adGuard = new AdGuard(
+    app.url,
+    findAppProperty(app, 'username'),
+    findAppProperty(app, 'password')
+  );
+
+  if (enable) {
+    await adGuard.disable();
+    return;
+  }
+
+  await adGuard.enable();
+};
+
+const processPiHole = async (app: ConfigAppType, enable: boolean) => {
+  const pihole = new PiHoleClient(app.url, findAppProperty(app, 'password'));
+
+  if (enable) {
+    await pihole.enable();
+    return;
+  }
+
+  await pihole.disable();
 };
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
