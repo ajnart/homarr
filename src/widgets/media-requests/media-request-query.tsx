3b368949baa1910b2fe435a788c6983fdae7c40f
Meier Lukas <meierschlumpf@gmail.com>
2023-06-10 15:13:23 +0200

    🐛 Fix issue with media request query

--- src/widgets/media-requests/media-request-query.tsx
+++ src/widgets/media-requests/media-request-query.tsx
@@ -3,7 +3,7 @@ import { api } from '~/utils/api';
 
 export const useMediaRequestQuery = () => {
   const { name: configName } = useConfigContext();
-  api.mediaRequest.all.useQuery(
+  return api.mediaRequest.all.useQuery(
     { configName: configName! },
     {
       refetchInterval: 3 * 60 * 1000,
