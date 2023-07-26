d4aa3e4e07caabefb455a9a6da25e1660cfdd695
ajnart <ajnart@pm.me>
2023-07-26 13:17:00 +0900

    🐛 Fix edit mode password working with lowercase

--- src/pages/api/configs/tryPassword.tsx
+++ src/pages/api/configs/tryPassword.tsx
@@ -5,8 +5,12 @@ function Post(req: NextApiRequest, res: NextApiResponse) {
   const { tried, type = 'password' } = req.body;
   // If the type of password is "edit", we run this branch to check the edit password
   if (type === 'edit') {
-    if (tried === process.env.EDIT_MODE_PASSWORD) {
-      process.env.DISABLE_EDIT_MODE = process.env.DISABLE_EDIT_MODE === 'true' ? 'false' : 'true';
+    if ((tried === process.env.EDIT_MODE_PASSWORD) !== undefined) {
+      if (process.env.DISABLE_EDIT_MODE?.toLowerCase() === 'true') {
+        process.env.DISABLE_EDIT_MODE = 'false';
+      } else {
+        process.env.DISABLE_EDIT_MODE = 'true';
+      }
       return res.status(200).json({
         success: true,
       });
