23b8c373724b3b04f0be4f825429266b37172560
Tagaishi <tagaishi@hotmail.ch>
2023-08-03 00:28:29 +0200

    ✨ Option to show time for a city

---
+++ src/server/api/routers/timezone.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { find } from 'geo-tz'
+
+import { createTRPCRouter, publicProcedure } from '../trpc';
+
+export const timezoneRouter = createTRPCRouter({
+  at: publicProcedure
+    .input(
+        z.object({
+        longitude: z.number(),
+        latitude: z.number(),
+    }))
+    .query(async ({ input }) => {
+        return find(input.latitude,input.longitude)[0];
+    }),
+})
--- src/widgets/date/DateTile.tsx
+++ src/widgets/date/DateTile.tsx
@@ -1,8 +1,10 @@
-import { Stack, Text, Title } from '@mantine/core';
+import { Flex, Stack, Text, Title } from '@mantine/core';
 import { useElementSize } from '@mantine/hooks';
 import { IconClock } from '@tabler/icons-react';
+import moment from 'moment-timezone';
 import dayjs from 'dayjs';
 import { useEffect, useRef, useState } from 'react';
+import { api } from '~/utils/api';
 
 import { useSetSafeInterval } from '../../hooks/useSetSafeInterval';
 import { defineWidget } from '../helper';
@@ -16,6 +18,18 @@ const definition = defineWidget({
       type: 'switch',
       defaultValue: false,
     },
+    enableTimezone:{
+      type: 'switch',
+      defaultValue: false,
+    },
+    timezoneLocation:{
+      type: 'location',
+      defaultValue: {
+        name: 'Paris',
+        latitude: 48.85341,
+        longitude: 2.3488,
+      },
+    }
   },
   gridstack: {
     minWidth: 1,
@@ -33,15 +47,16 @@ interface DateTileProps {
 }
 
 function DateTile({ widget }: DateTileProps) {
-  const date = useDateState();
+  const date = useDateState(widget.properties.enableTimezone ? widget.properties.timezoneLocation : undefined);
   const formatString = widget.properties.display24HourFormat ? 'HH:mm' : 'h:mm A';
   const { width, ref } = useElementSize();
 
   return (
-    <Stack ref={ref} spacing="xs" justify="space-around" align="center" style={{ height: '100%' }}>
+    <Flex ref={ref} display="flex" justify="space-around" align="center" h="100%" direction="column">
+      <Text size="md">{widget.properties.enableTimezone ? widget.properties.timezoneLocation.name : 'Local time'}</Text>
       <Title>{dayjs(date).format(formatString)}</Title>
       {width > 200 && <Text size="lg">{dayjs(date).format('dddd, MMMM D')}</Text>}
-    </Stack>
+    </Flex>
   );
 }
 
@@ -49,16 +64,18 @@ function DateTile({ widget }: DateTileProps) {
  * State which updates when the minute is changing
  * @returns current date updated every new minute
  */
-const useDateState = () => {
-  const [date, setDate] = useState(new Date());
+const useDateState = (location?: {latitude: number, longitude: number}) => {
+  //Gets a timezone from user input location. If location is undefined, then it means it's a local timezone so keep undefined
+  const timezone = location ? api.timezone.at.useQuery(location).data : undefined;
+  const [date, setDate] = useState(getNewDate(timezone));
   const setSafeInterval = useSetSafeInterval();
   const timeoutRef = useRef<NodeJS.Timeout>(); // reference for initial timeout until first minute change
   useEffect(() => {
     timeoutRef.current = setTimeout(() => {
-      setDate(new Date());
+      setDate(getNewDate(timezone));
       // Starts intervall which update the date every minute
       setSafeInterval(() => {
-        setDate(new Date());
+        setDate(getNewDate(timezone));
       }, 1000 * 60);
     }, getMsUntilNextMinute());
 
@@ -67,6 +84,18 @@ const useDateState = () => {
 
   return date;
 };
+
+//Returns a local date if no inputs or returns date from input zone
+const getNewDate = (timezone?: string) => {
+  if (timezone) {
+    //Get Date Time from region
+    const timezoneDate = moment(new Date()).tz(timezone).format();
+    //Remove mention of the UTC offset
+    const dateTruncated = timezoneDate.slice(0, timezoneDate.length-6);
+    return new Date(dateTruncated);
+  }
+  return new Date();
+}
 
 // calculates the amount of milliseconds until next minute starts.
 const getMsUntilNextMinute = () => {
