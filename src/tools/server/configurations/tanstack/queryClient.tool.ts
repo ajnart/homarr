4817c0c2670eecbe2d6a2e59bdd66eaa35ff16fc
Meier Lukas <meierschlumpf@gmail.com>
2023-08-05 12:57:29 +0200

    🐛 Updating search engine not working

--- src/tools/server/configurations/tanstack/queryClient.tool.ts
+++ src/tools/server/configurations/tanstack/queryClient.tool.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
