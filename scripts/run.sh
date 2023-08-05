2f2fdfb4385fbb945145df5bac6bb1d7224e598d
Manuel <manuel.ruwe@bluewin.ch>
2023-08-05 11:38:23 +0200

    ➕ Add sharp and surpress npm update notifier

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,7 @@ echo "Exporting hostname..."
 export NEXTAUTH_URL_INTERNAL="http://$HOSTNAME:7575"
 
 echo "Pushing database changes..."
+npm config set update-notifier false
 npx prisma db push --skip-generate
 
 echo "Starting production server..."
