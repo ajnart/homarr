2ef806ec5a68dbdcda97ec121fd93a5950b5f4cc
Manuel <30572287+manuel-rw@users.noreply.github.com>
2023-09-21 19:40:42 +0200

    ✅ Remove test

--- tests/pages/board/[slug].spec.ts
+++ tests/pages/board/[slug].spec.ts
@@ -1,14 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+
 import { SSRConfig } from 'next-i18next';
+
 import { ParsedUrlQuery } from 'querystring';
-import { beforeEach, describe, expect, it, vi, vitest } from 'vitest';
-import * as serverAuthModule from '~/server/auth';
-import { ConfigType } from '~/types/config';
+
+import { describe, expect, it, vitest } from 'vitest';
+
+import { getServerSideProps } from '../../../src/pages/board/[slug]';
 
 import * as configExistsModule from '../../../src/tools/config/configExists';
 import * as getFrontendConfigModule from '../../../src/tools/config/getFrontendConfig';
 import * as getServerSideTranslationsModule from '../../../src/tools/server/getServerSideTranslations';
-import { getServerSideProps } from '../../../src/pages/board/[slug]';
+
+import * as serverAuthModule from '~/server/auth';
+import { ConfigType } from '~/types/config';
 
 vitest.mock('./../../server/auth.ts', () => ({
   getServerAuthSession: () => null,
@@ -161,175 +166,6 @@ describe('[slug] page', () => {
       },
     });
     expect(serverAuthModule.getServerAuthSession).toHaveBeenCalledOnce();
-    expect(configExistsModule.configExists).toHaveBeenCalledOnce();
-    expect(getFrontendConfigModule.getFrontendConfig).toHaveBeenCalledOnce();
-  });
-
-  it('getServerSideProps should return success response when not allow guests but authenticated', async () => {
-    // arrange
-    vitest.spyOn(configExistsModule, 'configExists').mockReturnValue(true);
-    vitest.spyOn(getFrontendConfigModule, 'getFrontendConfig').mockReturnValueOnce(
-      Promise.resolve({
-        settings: {
-          access: {
-            allowGuests: false,
-          },
-          customization: {
-            colors: {
-              primary: 'red',
-              secondary: 'blue',
-              shade: 'green',
-            },
-          },
-        },
-      } as unknown as ConfigType)
-    );
-    vitest.spyOn(serverAuthModule, 'getServerAuthSession').mockReturnValueOnce(
-      Promise.resolve({
-        expires: '00',
-        user: {
-          colorScheme: 'dark',
-          id: 'ABC',
-          isAdmin: false,
-          language: 'DE',
-          email: 'test@homarr.dev',
-        },
-      })
-    );
-    vitest.spyOn(getServerSideTranslationsModule, 'getServerSideTranslations').mockReturnValueOnce(
-      Promise.resolve({
-        _nextI18Next: {
-          initialLocale: 'DE',
-          ns: ['common', 'my-testing-namespace'],
-          initialI18nStore: null,
-          userConfig: null,
-        },
-      })
-    );
-    vitest.stubEnv('DOCKER_PORT', '9999');
-    vitest.stubEnv('DOCKER_HOST', 'my-host');
-
-    // act
-    const response = await getServerSideProps({
-      req: {} as NextApiRequest,
-      res: {} as NextApiResponse,
-      query: {},
-      params: {
-        slug: 'my-authentication-board',
-      },
-      resolvedUrl: '/board/my-authentication-board',
-    });
-
-    // assert
-    expect(response).toEqual({
-      props: {
-        _nextI18Next: {
-          initialI18nStore: null,
-          initialLocale: 'DE',
-          ns: ['common', 'my-testing-namespace'],
-          userConfig: null,
-        },
-        config: {
-          settings: {
-            access: {
-              allowGuests: false,
-            },
-            customization: {
-              colors: {
-                primary: 'red',
-                secondary: 'blue',
-                shade: 'green',
-              },
-            },
-          },
-        },
-        dockerEnabled: true,
-        primaryColor: 'red',
-        primaryShade: 'green',
-        secondaryColor: 'blue',
-      },
-    });
-    expect(serverAuthModule.getServerAuthSession).toHaveBeenCalledOnce();
-    expect(configExistsModule.configExists).toHaveBeenCalledOnce();
-    expect(getFrontendConfigModule.getFrontendConfig).toHaveBeenCalledOnce();
-  });
-
-  it('getServerSideProps should return success response guests allowed and unauthenticated', async () => {
-    // arrange
-    vitest.spyOn(configExistsModule, 'configExists').mockReturnValue(true);
-    vitest.spyOn(getFrontendConfigModule, 'getFrontendConfig').mockReturnValueOnce(
-      Promise.resolve({
-        settings: {
-          access: {
-            allowGuests: true,
-          },
-          customization: {
-            colors: {
-              primary: 'red',
-              secondary: 'blue',
-              shade: 'green',
-            },
-          },
-        },
-      } as unknown as ConfigType)
-    );
-    vitest
-      .spyOn(serverAuthModule, 'getServerAuthSession')
-      .mockReturnValueOnce(Promise.resolve(null));
-    vitest.spyOn(getServerSideTranslationsModule, 'getServerSideTranslations').mockReturnValueOnce(
-      Promise.resolve({
-        _nextI18Next: {
-          initialLocale: 'DE',
-          ns: ['common', 'my-testing-namespace'],
-          initialI18nStore: null,
-          userConfig: null,
-        },
-      })
-    );
-    vitest.stubEnv('DOCKER_PORT', '9999');
-    vitest.stubEnv('DOCKER_HOST', 'my-host');
-
-    // act
-    const response = await getServerSideProps({
-      req: {} as NextApiRequest,
-      res: {} as NextApiResponse,
-      query: {},
-      params: {
-        slug: 'my-authentication-board',
-      },
-      resolvedUrl: '/board/my-authentication-board',
-    });
-
-    // assert
-    expect(response).toEqual({
-      props: {
-        _nextI18Next: {
-          initialI18nStore: null,
-          initialLocale: 'DE',
-          ns: ['common', 'my-testing-namespace'],
-          userConfig: null,
-        },
-        config: {
-          settings: {
-            access: {
-              allowGuests: true,
-            },
-            customization: {
-              colors: {
-                primary: 'red',
-                secondary: 'blue',
-                shade: 'green',
-              },
-            },
-          },
-        },
-        dockerEnabled: true,
-        primaryColor: 'red',
-        primaryShade: 'green',
-        secondaryColor: 'blue',
-      },
-    });
-    expect(serverAuthModule.getServerAuthSession).not.toHaveBeenCalled();
     expect(configExistsModule.configExists).toHaveBeenCalledOnce();
     expect(getFrontendConfigModule.getFrontendConfig).toHaveBeenCalledOnce();
   });
