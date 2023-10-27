4072ebc5a5c22b9cbb3c35cc29c680ad628a87c2
ajnart <ajnart@pm.me>
2023-10-27 15:53:35 +0200

    Header in manage layout redirects to `/b/` now

--- src/components/layout/Templates/ManageLayout.tsx
+++ src/components/layout/Templates/ManageLayout.tsx
@@ -89,7 +89,7 @@ export const ManageLayout = ({ children }: ManageLayoutProps) => {
             </Navbar.Section>
           </Navbar>
         }
-        header={<MainHeader showExperimental logoHref="/manage" leftIcon={burgerMenu} />}
+        header={<MainHeader showExperimental logoHref="/b/" leftIcon={burgerMenu} />}
         footer={
           <Footer height={25}>
             <Group position="apart" px="md">
