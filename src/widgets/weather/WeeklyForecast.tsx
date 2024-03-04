7f82e7755b115fc5d309ae9e492cbf36c7f04182
hillaliy <hillaliy@me.com>
2024-03-04 06:35:15 +0200

    fix space between

--- src/widgets/weather/WeeklyForecast.tsx
+++ src/widgets/weather/WeeklyForecast.tsx
@@ -6,7 +6,7 @@ import { WeatherIcon } from './WeatherIcon';
 const Forecast = ({ daily, getPerferedUnit, widget }) => {
   const { width } = useElementSize();
   return (
-    <Flex align="center" direction="row">
+    <Flex align="center" direction="row" justify="space-between" w="100%" px="sm">
       {daily.time.slice(0, widget.properties.forecastDays).map((time: any, index: number) => (
         <Card key={index} padding="0.25rem">
           <Flex direction="column" align="center">
