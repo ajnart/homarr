d577b1adbfb23c359189ca29e689174bb35c852d
Carlos Carneiro <carloscarneiropmw@gmail.com>
2024-04-09 18:11:02 -0300

    new api endpoints

--- src/server/api/routers/bitcoin/router.ts
+++ src/server/api/routers/bitcoin/router.ts
@@ -2,27 +2,53 @@ import { z } from 'zod';
 
 import { createTRPCRouter, publicProcedure } from '../../trpc';
 
-const btcPriceSchema = z.object({
-  time: z.number(),
-  USD: z.number(),
-  EUR: z.number(),
-  GBP: z.number(),
-  CAD: z.number(),
-  CHF: z.number(),
-  AUD: z.number(),
-  JPY: z.number(),
+const cryptoPriceSchema = z.object({
+  id: z.number().optional(),
+  symbol: z.string(),
+  name: z.string(),
+  links: z.object({
+    homepage: z.string().optional(),
+    whitepaper: z.string().optional(),
+    twitter_screen_name: z.string().optional(),
+    facebook_username: z.string().optional(),
+    subreddit_url: z.string().optional(),
+  }),
+  image: z.object({
+    thumb: z.string().optional(),
+    small: z.string().optional(),
+    large: z.string().optional(),
+  }),
+  market_cap_rank: z.number(),
+  market_data: z.object({
+    current_price: z.array(z.object({})),
+    ath: z.array(z.object({})),
+    atl: z.array(z.object({})),
+    total_volume: z.array(z.object({})),
+    price_change_percentage_24h: z.number(),
+    price_change_percentage_7d: z.number(),
+    price_change_percentage_30d: z.number(),
+  }),
 });
 
-const recommendedFeesSchema = z.object({
-  fastestFee: z.number(),
-  halfHourFee: z.number(),
-  hourFee: z.number(),
-  minimumFee: z.number(),
-});
-
-const initialDataSchema = z.object({
-  price: btcPriceSchema,
-  recommendedFees: recommendedFeesSchema,
+const cryptoMarketChart = z.object({
+  prices: z.array(
+    z.object({
+      0: z.number().optional(),
+      1: z.number(),
+    })
+  ),
+  marketCaps: z.array(
+    z.object({
+      0: z.date().optional(),
+      1: z.number(),
+    })
+  ),
+  totalVolume: z.array(
+    z.object({
+      0: z.date().optional(),
+      1: z.number(),
+    })
+  ),
 });
 
 export const bitcoinRouter = createTRPCRouter({
@@ -33,27 +59,26 @@ export const bitcoinRouter = createTRPCRouter({
   getInitialData: publicProcedure.output(initialDataSchema).query(async () => getInitialData()),
 });
 
-export type BtcPrice = z.infer<typeof btcPriceSchema>;
-export type RecommendedFees = z.infer<typeof recommendedFeesSchema>;
+export type CryptoPrice = z.infer<typeof cryptoPriceSchema>;
 
-export const getRecommendedFees = async () => {
-  const res = await fetch(`https://mempool.space/api/v1//fees/recommended`);
-  return recommendedFeesSchema.parse(await res.json());
+export const getCoinGeckoCryptoData = async (crypto = 'bitcoin') => {
+  const res = await fetch(`https://api.coingecko.com/api/v3/coins/${crypto}`);
+  return cryptoPriceSchema.parse(await res.json());
 };
-export const fetchHistoricalPrice = async (currency:string, timestamp: number) => {
-  const res = await fetch(`https://mempool.space/api/v1/historical-price?currency=${currency}&timestamp=${timestamp}`);
-  return btcPriceSchema.parse(await res.json());
+export const getCoinGeckoCryptoMarketChart = async (
+  fiatCurrency = "usd",
+  days = 7
+) => {
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${fiatCurrency}&days=${days}`
+  );
+  return cryptoPriceSchema.parse(await res.json());
 };
-export const fetchPrice = async () => {
-  const res = await fetch(`https://mempool.space/api/v1/prices`);
-  return btcPriceSchema.parse(await res.json());
-};
-
 export const getInitialData = async () => {
-  const price = await fetchPrice();
-  const recommendedFees = await getRecommendedFees();
+  const cryptoData = await getCoinGeckoCryptoData();
+  const marketData = await getCoinGeckoCryptoMarketChart();
   return {
-    price: btcPriceSchema.parse(price),
-    recommendedFees: recommendedFeesSchema.parse(recommendedFees),
+    cryptoData,
+    marketData
   };
 };
