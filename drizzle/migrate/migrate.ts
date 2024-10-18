67432d518aa86fa9a7015de0d9966279ff8741c0
Meier Lukas <meierschlumpf@gmail.com>
2024-10-18 20:45:16 +0200

    fix: db migration not working

--- drizzle/migrate/migrate.ts
+++ drizzle/migrate/migrate.ts
@@ -1,14 +1,14 @@
 // This file is used to migrate the database to the current version
 // It is run when the docker container starts
-import Database from 'better-sqlite3';
-import dotenv from 'dotenv';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+const Database = require('better-sqlite3');
+const path = require('path');
+const dotenv = require('dotenv');
+const { drizzle } = require('drizzle-orm/better-sqlite3');
+const { migrate } = require('drizzle-orm/better-sqlite3/migrator');
 
 const migrationsFolder = process.argv[2] ?? '../drizzle';
 
-dotenv.config({ path: __dirname + '/../.env' });
-
+dotenv.config({ path: path.join(__dirname, '/../.env') });
 const sqlite = new Database(process.env.DATABASE_URL!.replace('file:', ''));
 
 const db = drizzle(sqlite);
