ff5ecd7140299f115f583c632af2cd36a56344e8
TyxTang <2222002219@qq.com>
2024-11-11 21:43:02 +0800

    refactor: Optimize variable

--- src/server/api/routers/media-request.ts
+++ src/server/api/routers/media-request.ts
@@ -161,9 +161,9 @@ const constructAvatarUrl = (appUrl: string, avatar: string, isVersionValid: bool
     : `${appUrl}/${avatar}`;
 };
 
-const compareVersions = (v1: string, v2: string): number => {
-  const v1Parts = v1.split('.').map(Number);
-  const v2Parts = v2.split('.').map(Number);
+const compareVersions = (currentVersion: string, targetVersion: string): number => {
+  const v1Parts = currentVersion.split('.').map(Number);
+  const v2Parts = targetVersion.split('.').map(Number);
   for (let i = 0; i < Math.max(v1Parts.length, v2Parts.length); i++) {
     const v1Part = v1Parts[i] || 0;
     const v2Part = v2Parts[i] || 0;
