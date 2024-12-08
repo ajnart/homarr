85547277d1daf7065fffe60d516f4d5dd0b67878
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-12-08 20:06:15 +0100

    fix: iframes javascript content (#2218)

--- src/widgets/iframe/IFrameTile.tsx
+++ src/widgets/iframe/IFrameTile.tsx
@@ -5,6 +5,20 @@ import { useTranslation } from 'next-i18next';
 import { defineWidget } from '../helper';
 import { IWidget } from '../widgets';
 
+function sanitizeUrl(url: string) {
+  let parsedUrl: URL;
+  try {
+    parsedUrl = new URL(url);
+  } catch (e) {
+    return 'about:blank';
+  }
+  if (['http:', 'https:'].includes(parsedUrl.protocol)) {
+    return parsedUrl.href;
+  } else {
+    throw new Error(`Protocol '${parsedUrl.protocol}' is not supported. Use HTTP or HTTPS.`);
+  }
+}
+
 const definition = defineWidget({
   id: 'iframe',
   icon: IconBrowser,
@@ -116,9 +130,8 @@ function IFrameTile({ widget }: IFrameTileProps) {
   return (
     <Container h="100%" w="100%" maw="initial" mah="initial" p={0}>
       <iframe
-        sandbox="" // Disables js execution see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#sandbox
         className={classes.iframe}
-        src={widget.properties.embedUrl}
+        src={sanitizeUrl(widget.properties.embedUrl)}
         title="widget iframe"
         allow={allowedPermissions.join(' ')}
       >
